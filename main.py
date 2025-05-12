6af79c96ab0d3728628c63e0d9a1baee58b9e976
Артём <artemnodvel22@gmail.com>
2025-05-13 00:08:44 +0500

    разметки для регистрации

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
 from data import db_session
+from loginform import  LoginForm
+from roomform import  RoomForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -7,7 +9,23 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 def main():
     db_session.global_init("db/quiz.db")
-    app.run()
+
+    @app.route('/')
+    @app.route('/index')
+    def index():
+        form = RoomForm()
+        if form.validate_on_submit():
+            return redirect('/success')
+        return render_template('index.html', title='Главная', form=form)
+
+    @app.route('/login', methods=['GET', 'POST'])
+    def login():
+        form = LoginForm()
+        if form.validate_on_submit():
+            return redirect('/success')
+        return render_template('login.html', title='Авторизация', form=form)
+
+    app.run(port=8080, host='127.0.0.1')
 
 
 if __name__ == '__main__':
---
+++ roomform.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class RoomForm(FlaskForm):
+    username = StringField('Отоброжаемое имя', validators=[DataRequired()])
+    password = PasswordField('Тэг комнаты', validators=[DataRequired()])
+    submit = SubmitField('Подключиться')
