f92f0387c3ab4526b1ecbf488282de60a6697b9c
Артём <artemnodvel22@gmail.com>
2025-05-13 00:42:37 +0500

    Полная регистрация + авторизация

--- forms/registerform.py
+++ forms/registerform.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired
 
 
 class RegisterForm(FlaskForm):
-    login = EmailField('Логин', validators=[DataRequired()])
+    login = StringField('Логин', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
     password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
     nickname = StringField('Отображаемое имя', validators=[DataRequired()])
