6f305c4193cb2b885b80012673c87328f0d09fd8
Артём <artemnodvel22@gmail.com>
2025-05-15 22:20:30 +0500

    Перенос данных с урока

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, redirect
 from data import db_session
-from forms.loginform import  LoginForm
-from forms.roomform import  RoomForm
+from forms.loginform import LoginForm
+from forms.roomform import RoomForm
 from forms.registerform import RegisterForm
 from data.users import User
+from flask_login import login_user, LoginManager, logout_user, login_required
 
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
@@ -17,17 +20,35 @@ def main():
     def index():
         form = RoomForm()
         if form.validate_on_submit():
-            return redirect('/success')
+            return redirect(f'/room{form.password}')
         return render_template('index.html', title='Главная', form=form)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        db_sess = db_session.create_session()
+        return db_sess.query(User).get(user_id)
+
+    @app.route('/logout')
+    @login_required
+    def logout():
+        logout_user()
+        return redirect("/")
 
     @app.route('/login', methods=['GET', 'POST'])
     def login():
         form = LoginForm()
         if form.validate_on_submit():
-            return redirect('/success')
+            db_sess = db_session.create_session()
+            user = db_sess.query(User).filter(User.login == form.username.data).first()
+            if user and user.check_password(form.password.data):
+                login_user(user, remember=True)
+                return redirect("/index")
+            return render_template('login.html',
+                                   message="Неправильный логин или пароль",
+                                   form=form)
         return render_template('login.html', title='Авторизация', form=form)
 
-    @app.route('/register', methods=['GET', 'POST'])
+    @app.route('/register')
     def reqister():
         form = RegisterForm()
         if form.validate_on_submit():
